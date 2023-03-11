3bbd2733e4864c693defef7731bf5cbb88f0dfee
Chase <chsmatherly@gmail.com>
2023-03-10 16:49:31 -0800

    added checkbox functionality

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ app.secret_key = 'Speak friend and enter'
 
 @app.route('/')
 def index():
+    # Clear session to account for checkbox logic persisting in session
+    session.clear()
     return render_template('index.html')
 
 @app.route('/process', methods=['post'])
@@ -14,6 +16,14 @@ def result_post():
     session['location'] = request.form['location']
     session['name'] = request.form['name']
     session['comment'] = request.form['comment']
+    session['radio'] = request.form['radio']
+    if 'goober' in request.form:
+        session['goober'] = request.form['goober']
+    if 'ready' in request.form:
+        session['ready'] = request.form['ready']
+
+    # session['goober'] = request.form['goober']
+    # session['ready'] = request.form['ready']
     print(session)
     return redirect('/result')
 
